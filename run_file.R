63f6251653539e4bc6ae30b67e93f40b05aa863b
Laura Birks <75485920+loz-birks@users.noreply.github.com>
2026-03-26 14:11:22 +0000

    add loop to run_file

--- run_file.R
+++ run_file.R
@@ -5,15 +5,31 @@ source("helper/generate_plot.R")
 # Save to Excel
 # create a workbook
 wb <- createWorkbook()
-# add a worksheet to the workbook
-addWorksheet(wb, "Var1")
-# add the plot
-insertPlot(wb, "Var1", 
-           width = 5, height = 4, units = "in", 
-           # specify where the plot should be (here I move it relative to the data)
-           startRow = 1, startCol = ncol(df) + 2)
-# add the data
-writeData(wb, "Var1", x = df, startRow = 1, startCol = 1)
+
+# then iterate over all possible cyl values
+for (i in c(4, 6, 8)){
+  
+  # 1. add a worksheet to the workbook
+  # define sheet name
+  sheet_name <- paste0("Sheet_", i)
+  # create sheet
+  addWorksheet(wb, sheet_name)
+  
+  # 2. add the data
+  # define the data
+  data <- data_fn(i)
+  # put it in the excel
+  writeData(wb, sheet_name, x = data, startRow = 1, startCol = 1)
+  
+  # 3. add the plot
+  # print the plot 
+  plot_fn(i)
+  # put it in the excel
+  insertPlot(wb, sheet_name, 
+             width = 5, height = 4, units = "in", 
+             # specify where the plot should be (here I move it relative to the data)
+             startRow = 1, startCol = ncol(data) + 2)
+}
+
 # save the workbook
 saveWorkbook(wb, paste0("outputs/example_", format(Sys.Date(), "%Y%m%d"), ".xlsx"), overwrite = TRUE)
-             
